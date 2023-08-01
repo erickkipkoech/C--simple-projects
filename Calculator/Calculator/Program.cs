db3b2364fdfd481729787e1a1c29affe2ef5309d
erickkipkoech <cheruiyotkipkoech612@gmail.com>
2023-08-02 01:34:26 +0300

    first commit

---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+int num1,num2,result;
+string operation;
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Please Enter the first number");
+num1 =Convert.ToInt32( Console.ReadLine());
+Console.WriteLine("Please Enter the second number");
+num2=Convert.ToInt32( Console.ReadLine());
+Console.WriteLine("What type of operation would you like to do?");
+Console.WriteLine("Please Enter a for addition, s for subtraction,m for multiplication,d for division");
+operation = Console.ReadLine();
+switch (operation)
+{
+    case "a":result = num1 + num2; Console.WriteLine(result);break;
+    case "m":result = num1 * num2; Console.WriteLine(result);break;
+    case "s":result = num1 - num2; Console.WriteLine(result);break;
+    case "d":result = num1 / num2; Console.WriteLine(result);break;
+    default: Console.WriteLine("You have Chosen the wrong letter "+operation);break;
+}
---
+++ Dice/Dice/Program.cs
@@ -0,0 +1,50 @@
+﻿// See https://aka.ms/new-console-template for more information
+int playerRandomNum;
+int enemyRandomNum;
+
+int playerPoints=0;
+int enemyPoints=0;
+Random random=new Random();
+for(int i = 0; i < 10; i++)
+{
+Console.WriteLine("Press any Key to roll the dice");
+    Console.ReadKey();
+
+    playerRandomNum = random.Next(1,8);
+    Console.WriteLine("You rolled a " + playerRandomNum);
+
+    Console.WriteLine("...");
+    System.Threading.Thread.Sleep(1000);
+
+    enemyRandomNum = random.Next(1,8);
+    Console.WriteLine("Enemy AI rolled a " + enemyRandomNum);
+
+    if (playerRandomNum > enemyRandomNum)
+    {
+        playerPoints++;
+        Console.WriteLine("Player wins this round");
+    }else if(enemyRandomNum>playerRandomNum){
+        enemyPoints++;
+        Console.WriteLine("Enemy wins this round");
+    }
+    else
+    {
+        Console.WriteLine("Draw!");
+    }
+
+    Console.WriteLine("The score is now -Player "+playerPoints+". -Enemy "+enemyPoints);
+    Console.WriteLine("-----------------------------------------------------------");
+
+    if (playerPoints > enemyPoints)
+    {
+        Console.WriteLine("You win!");
+    }else if (enemyPoints > playerPoints)
+    {
+        Console.WriteLine("You lose!");
+    }
+    else
+    {
+        Console.WriteLine("It's a draw!");
+    }
+}
+
